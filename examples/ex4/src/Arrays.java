66ccf8b2939c343cdb1015ac27dac1e9fd568629
denis-selutin <silver1115@mail.ru>
2015-05-16 13:50:03 +0300

    Lection 3. StringBuilder пример

--- examples/ex4/src/Arrays.java
+++ examples/ex4/src/Arrays.java
@@ -33,5 +33,13 @@ public class Arrays {
             }
             System.out.println();
         }
+
+        lab1: for (int i=0; i<10; i++) {
+            for (int j=0; j<10; j++) {
+                if (j>=5 && i>5)
+                    break lab1;
+                System.out.println(i + " / " + j);
+            }
+        }
     }
 }
